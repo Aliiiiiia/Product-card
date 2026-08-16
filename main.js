30ed1c38ee99957cd7a33f1b7b5de7ca6c7cfd56
Aliya <a-altyn@list.ru>
2026-08-16 11:56:54 +0300

    Исправить заголовок

--- main.js
+++ main.js
@@ -1,45 +1,51 @@
 // Покраска всех карточек
 
-const productCards = document.querySelectorAll('.card');
-const changeColorAllCardButton = document.querySelector('#change-color-all-card-button');
-const lightblueColorHash = '#ADD8E6';
-const blueColorHash = '#7c7cea';
+const productCards = document.querySelectorAll(".card");
+const changeColorAllCardButton = document.querySelector(
+  "#change-color-all-card-button",
+);
+const lightblueColorHash = "#ADD8E6";
+const blueColorHash = "#7c7cea";
 
-changeColorAllCardButton.addEventListener('click', () => {
-  productCards.forEach((card) => card.style.backgroundColor = lightblueColorHash)
-})
+changeColorAllCardButton.addEventListener("click", () => {
+  productCards.forEach(
+    (card) => (card.style.backgroundColor = lightblueColorHash),
+  );
+});
 
 // Покраска первой карточки
 
-const firstProductCard = document.querySelector('.card');
-const changeFirstCardColorButton = document.querySelector('#change-first-card-color-button');
+const firstProductCard = document.querySelector(".card");
+const changeFirstCardColorButton = document.querySelector(
+  "#change-first-card-color-button",
+);
 
-changeFirstCardColorButton.addEventListener('click', () => {
+changeFirstCardColorButton.addEventListener("click", () => {
   firstProductCard.style.backgroundColor = blueColorHash;
-})
+});
 
 // Открыть google
 
-const openGoogleButton = document.querySelector('#open-google');
-openGoogleButton.addEventListener('click', openGoogle) 
+const openGoogleButton = document.querySelector("#open-google");
+openGoogleButton.addEventListener("click", openGoogle);
 
 function openGoogle() {
-  const answer = confirm('Вы действительно хотите открыть Google?');
+  const answer = confirm("Вы действительно хотите открыть Google?");
 
   if (answer === true) {
-    window.open('https://www.google.com');
+    window.open("https://www.google.com");
   } else {
-  return;
-  }  
+    return;
+  }
 }
 
 // Вывод консоль лог
 
-const outputLogButton = document.querySelector('#output-console-log');
+const outputLogButton = document.querySelector("#output-console-log");
 
-outputLogButton.addEventListener('click', () => outputConsoleLog('ДЗ №4'))
+outputLogButton.addEventListener("click", () => outputConsoleLog("ДЗ №4"));
 
 function outputConsoleLog(messege) {
-  alert(messege)
-  console.log(messege) 
+  alert(messege);
+  console.log(messege);
 }
